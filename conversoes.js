1a01dce90b4133d90ae1aac90e90441470903384
Marcos Emanuel <marcosemanuel@duck.com>
2022-05-06 22:40:08 -0300

    Fundamentos do JavaScript - Alura

---
+++ conversoes.js
@@ -0,0 +1,19 @@
+// tipo de dado
+// booleanos
+
+// conversão implicita
+const numero = 456;
+const numeroString = Number("456");
+
+// console.log(numero == numeroString);
+
+// console.log(numero + numeroString);
+
+
+
+// conversão explicita
+
+// Number(); -> Converter String para numero
+// String(); -> Converter Numero para String
+
+console.log(numero + numeroString);
---
+++ type_number.js
@@ -0,0 +1,23 @@
+// tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operaçãoMatematica = primeiroNumero / segundoNumero;
+
+// numero flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
+
+// NaN -> Not a Number (não é um numero)
+
+const alura = "Alura";
+
+console.log(alura * primeiroNumero);
