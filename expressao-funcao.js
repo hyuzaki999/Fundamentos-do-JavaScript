ba6d0929310bba07b7163cd3bcc8df7f58410f1b
Marcos Emanuel <marcosemanuel@duck.com>
2022-05-09 18:54:53 -0300

    Fundamentos do JavaScript, Funções - Alura

--- expressao-funcao.js
+++ expressao-funcao.js
@@ -30,5 +30,6 @@ console.log(soma(1, 1));
 
 
 
+// Expressões de função, uma maneira diferente de montar funções usando variáveis do tipo const e chamando-as pelo nome.
 // Lembrando que é necessário que o programa passe pela variável antes de podermos chamá-la, já que não há suporte à hoisting.
 // Hoisting: o JavaScript analisa todo o código procurando por variáveis declaradas com var e funções para trazer tais declarações para o início do código.
