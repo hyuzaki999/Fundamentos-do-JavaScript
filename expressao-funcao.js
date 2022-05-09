aab0bce75e89a92fac97731283c3de1c80c81ee8
Marcos Emanuel <marcosemanuel@duck.com>
2022-05-09 18:47:48 -0300

    Fundamentos do JavaScript, Funções - Alura

--- expressao-funcao.js
+++ expressao-funcao.js
@@ -27,3 +27,8 @@ function apresentar() {
 console.log(soma(1, 1));
 // const soma = function(num1, num2) {
 //     return num1 + num2};
+
+
+
+// Lembrando que é necessário que o programa passe pela variável antes de podermos chamá-la, já que não há suporte à hoisting.
+// Hoisting: o JavaScript analisa todo o código procurando por variáveis declaradas com var e funções para trazer tais declarações para o início do código.
